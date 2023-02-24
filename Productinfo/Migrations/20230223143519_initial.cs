9b3ad05ba4bb3bb138f7c823dc259b4d6ad8a3ce
Mangesh-929 <122014047+Mangesh-929@users.noreply.github.com>
2023-02-24 12:19:42 +0530

    initial commit

---
+++ Productinfo/Migrations/20230223143519_initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    productId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    productName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    price = table.Column<int>(type: "int", nullable: false),
+                    prodDesc = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.productId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ Productinfo/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDApp.Models
+{
+    public class Product
+    {
+        [Key]
+         public int productId { get; set; }
+        [Required]
+        public string productName { get; set; }
+        public  int price { get; set; }
+        public string prodDesc { get; set; }
+
+    }
+}
