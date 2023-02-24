e566366d97dc64f765aee680ed36d4a108eda302
Mangesh-929 <122014047+Mangesh-929@users.noreply.github.com>
2023-02-24 12:17:57 +0530

    initial commit

---
+++ Productinfo/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using CRUDApp.Data;
+using CRUDApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public ProductController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = context.Products.ToList();
+            return View(result);
+
+        }
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                var pro = new Product()
+                {
+                    productName = model.productName,
+                    price=model.price,
+                    prodDesc=model.prodDesc
+                };
+                context.Products.Add(pro);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Empty Field cannot be submit";
+                return View(model);
+            }
+               
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            var pro = context.Products.SingleOrDefault(p => p.productId == id);
+            context.Products.Remove(pro);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult UpdateProduct(int id)
+        {
+            var pro = context.Products.SingleOrDefault(p => p.productId == id);
+            var result = new Product()
+            {
+                productId = pro.productId,
+                productName = pro.productName,
+                price = pro.price,
+                prodDesc = pro.prodDesc
+            };
+            return View(result);
+        }
+        
+        [HttpPost]
+        public IActionResult UpdateProduct(Product model)
+        {
+            var pro = new Product()
+            {
+                productId=model.productId,
+                productName = model.productName,
+                price = model.price,
+                prodDesc = model.prodDesc
+            };
+            context.Products.Update(pro);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Productinfo/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CRUDApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CRUDApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opt) : base(opt)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        { }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
